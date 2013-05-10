352880031711e3873c79742d96f5ba31d7cf32b4
tek911 <felix.simmons@gmail.com>
2013-05-10 16:50:13 -0700

    second committ. blind elephant and wpscan done

--- wordpress.sh
+++ wordpress.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+if [ "$#" == "0" ]; then
+    echo "proper syntax $0 [host url]"
+    exit 1
+fi
+
+echo "Running Blind Elephant Scanning:"
+BlindElephant.py $1 wordpress > ./output/wordpress/blindelephant.out
+
+echo "Running WPScan Scanning:"
+wpscan --url $1 --enumerate p --proxy 127.0.0.1:8080
+exit 0
