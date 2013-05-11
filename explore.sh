015f6712aff9467cacee71243a34d09319871740
James Wickett <wickett@gmail.com>
2013-05-10 22:04:39 -0700

    added OS detection to nmap scan

--- explore.sh
+++ explore.sh
@@ -1,11 +1,18 @@
 !#/bin/bash
 
 #Do a zone transfer
-dig @10.42.6.2 sec642.org axfr
+dig @10.42.6.2 sec642.org axfr | tee output/dns_zone_transfer
 
-#Look for any hidden web services not in dns
-sudo nmap -oX -sS 10.42.6.0/24 -oX output.xml
+#Quick look at the network
+sudo nmap -sL 10.42.6.0/24
 
-#Look for OS detection
-sudo nmap -sS 10.42.6.0/24
+#Look for any web services
+sudo nmap -oX output/nmap_fast_output.xml -F 10.42.6.0/24
+
+#OS detection
+sudo nmap -O 10.42.6.0/24
+
+#Full nmap
+sudo nmap -oX output/nmap_full_output.xml -sS 10.42.6.0/24
+
 
