093a9994a7644b2f2571e16d0e35c81ddd95ac36
tek911 <felix.simmons@gmail.com>
2013-05-10 17:14:14 -0700

    third comitt. wrapping wordpress.sh for the night. nmap line commented out.

--- wordpress.sh
+++ wordpress.sh
@@ -10,4 +10,6 @@ BlindElephant.py $1 wordpress > ./output/wordpress/blindelephant.out
 
 echo "Running WPScan Scanning:"
 wpscan --url $1 --enumerate p --proxy 127.0.0.1:8080
+
+#nmap --script=http-wordpress-plugins --script-args http-wordpress-plugins.root="/",http-wordpress-plugins.search=500 http://captaincreative.com.au/
 exit 0
